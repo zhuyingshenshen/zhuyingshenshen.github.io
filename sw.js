bcb6a1da92cffcb45b847ef4f8725aeceec90421
zhuyingshenshen <867903119@qq.com>
2024-11-25 12:36:22 +0800

    Site updated: 2024-11-25 12:36:22

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732509203502";
+const VERSION = "1732509365532";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
