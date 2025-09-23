6297d5dea86e6c0ef71360c91ee8c4dd86a0cba8
zhuyingshenshen <867903119@qq.com>
2025-09-23 18:49:20 +0800

    Site updated: 2025-09-23 18:49:19

--- cgtest/test1/js/square.js
+++ cgtest/test1/js/square.js
@@ -28,7 +28,7 @@ window.onload = function init(){
 
 	// Configure WebGL
 	gl.viewport( 0, 0, canvas.width, canvas.height );
-	gl.clearColor( 1.0, 0.0, 0.0, 1.0 );
+	gl.clearColor( 0.0, 0.0, 0.0, 0.0 );
 
 	// Load shaders and initialize attribute buffers
 	var program = initShaders( gl, "vertex-shader", "fragment-shader" );
