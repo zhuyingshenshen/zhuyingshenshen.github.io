9ed24fd23b92cffc5b7aa082a5818c006b1ba28c
zhuyingshenshen <867903119@qq.com>
2026-01-15 16:37:28 +0800

    Site updated: 2026-01-15 16:37:28

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1768463515902";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1768466245107";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
